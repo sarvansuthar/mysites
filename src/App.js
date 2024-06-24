d0f5f8c02b8571b2352bb8d3e00a7836976b2618
sarvansuthar <sutharsarvan29>
2024-06-24 11:57:19 +0530

    second

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+function App() {
+  return (
+    <>
+    <video autoPlay muted loop>
+      <source src="/Y2meta.app - This is 4K Anime _ Garden Of Words _ Beauty Of 4k Anime-(1080p).mp4" type="video/mp4"/>
+    </video>
+    <header>
+        <a href="#">News</a>
+        <a href="#">Events</a>
+        <a href="#">Giving</a>
+        <a href="#">Alumni</a>
+        <a href="#" className='extra'>Info For </a>
+        <a href="#">Who We Are</a>
+        <a href="#">Campus & City</a>
+        <a href="#">Academic Life</a>
+        <a href="#">Research</a>
+        <a href="#">Admission & Aid</a>
+        <a href="#">🔍</a>
+    </header>
+    <div className='page2'>
+    <div className="left2">
+      <p className='above2'>
+        Finding a reliable freelancer is really hard.Hiring a full-time designer is too expensive.
+        <div className="colorful2">
+          Limitless makes email easy.
+        </div>
+      </p>
+      <div className="lower2">
+        Our high-quality email design serive is sress-free and affordable.We give you access to world-class design,unlimeited projects,and limitless revisions for one flat monthly fee.
+      </div>
+      <button className='button2'>How it works</button>
+w
+    </div>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt8Pvse3Itl-0y8fgFO12PY_-hENOcCW7wxg&s" alt="" />
+    
+    </div>
+    </>
+  );
+}
+
+export default App;
